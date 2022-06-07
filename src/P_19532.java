c73d0b11bc2e0a01da2b5ba8f5b1f412ed004adc
Donggeun-Do <81558014+Donggeun-Do@users.noreply.github.com>
2022-06-07 17:55:46 +0900

    미완성 2문제 커밋 (1339, 19532)

---
+++ src/P_19532.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P_19532 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x_1 = input.nextInt();
+        int y_1 = input.nextInt();
+        int result_1 = input.nextInt();
+        int x_2 = input.nextInt();
+        int y_2 = input.nextInt();
+        int result_2 = input.nextInt();
+
+
+    }
+}
