c700988178e003db7c25167379b823eb555853f7
도동근 <donggeun_do@naver.com>
2022-06-28 21:45:15 +0900

    문제 1003 작성중

---
+++ src/P_1003.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class P_1003 {
+    static int count_zero;
+    static int count_one;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int times = Integer.parseInt(br.readLine());
+        int[] arr = new int[times];
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int j : arr) {
+            count_zero = 0;
+            count_one = 0;
+
+            fibonacci(j);
+
+            bw.write(count_zero + " " + count_one + "\n");
+            bw.flush();
+        }
+        bw.close();
+    }
+
+    public static void fibonacci(int number) {
+        if (number==0) {
+            count_zero++;
+        } else if (number==1) {
+            count_one++;
+        } else {
+            fibonacci(number-1);
+            fibonacci(number-2);
+        }
+    }
+}
--- src/P_25285.java
+++ src/P_25285.java
@@ -13,23 +13,23 @@ public class P_25285 {
         }
         input.close();
 
-        for(int i=0; i<times; i++) {
-            if(height < 140.1) {
-                System.out.println("6넣음");
-                result[i] =  6;
-                break;
-            } else if (height >= 140.1 && height<146) {
-                System.out.println("5넣음");
-                result[i] =  5;
-                break;
-            } else if ((height >= 146 && height<159) || height >= 204) {
-                System.out.println("4넣음");
-                result[i] =  4;
-            } else {
-                result[i] =  777;
-            }
-            System.out.println(result[i]);
-        }
+//        for(int i=0; i<times; i++) {
+//            if(height < 140.1) {
+//                System.out.println("6넣음");
+//                result[i] =  6;
+//                break;
+//            } else if (height >= 140.1 && height<146) {
+//                System.out.println("5넣음");
+//                result[i] =  5;
+//                break;
+//            } else if ((height >= 146 && height<159) || height >= 204) {
+//                System.out.println("4넣음");
+//                result[i] =  4;
+//            } else {
+//                result[i] =  777;
+//            }
+//            System.out.println(result[i]);
+//        }
 
 
     }
